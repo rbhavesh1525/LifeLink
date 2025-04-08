899eefa9e397bf575603d791054955feecec6e81
satyam-trimale <trimalesatyam@gmail.com>
2025-04-09 01:33:34 +0530

    Add user page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import {HospitalHome,UserHomepage,UserSignup,UserSignin,PatientTransferRecords,S
 import {Navbar} from "./Components/CompIndex"
 import {Routes,Route,BrowserRouter} from "react-router-dom"
 import { ToastContainer } from "react-toastify";
+import Footer from './Components/Footer';
 function App() {
   
 
@@ -13,21 +14,21 @@ function App() {
 
     <Routes>
 
-      <Route path='/'element ={<><Navbar/><UserHomepage/></>}/>
-      <Route path="/hospital-homepage" element ={<><Navbar/><HospitalHome/></>}/>
-      <Route path='/signing-as' element={<SigninAs/>}/>
+      <Route path='/' element={<SigninAs/>}/>
+      <Route path='/user-homepage'element ={<><Navbar/><UserHomepage/><Footer/></>}/>
+      <Route path="/hospital-homepage" element ={<><Navbar/><HospitalHome/><Footer/></>}/>
       <Route path='/user-signup' element={<UserSignup/>}/>
       <Route path='/user-signin' element={<UserSignin/>}/>
       <Route path='/hospital-signup' element={<HospitalSignup/>}/>
       <Route path='/hospital-signin' element={<HospitalSignin/>}/>
       <Route path='/ambulance-signup' element={<AmbulanceSignup/>}/> 
       <Route path='/ambulance-signin' element={<AmbulanceSignin/>}/>
-      <Route path='/doctor-availability' element={<><Navbar/><DoctorAvailability/></>}/>
-      <Route path='/patient-transfer-records' element={<><Navbar/><PatientTransferRecords/></>}/>
-      <Route path='/staff-info' element={<><Navbar/><Staffinfo/></>}/>
-      <Route path='/transfer-patient' element={<><Navbar/><TransferPatient/></>}/>
-      <Route path='/update-bed-status' element={<><Navbar/><UpdateBedStatus/></>}/>
-      <Route path='/update-hospital-profile' element={<><Navbar/><UpdateHospitalProfile/></>}/>
+      <Route path='/doctor-availability' element={<><Navbar/><DoctorAvailability/><Footer/></>}/>
+      <Route path='/patient-transfer-records' element={<><Navbar/><PatientTransferRecords/><Footer/></>}/>
+      <Route path='/staff-info' element={<><Navbar/><Staffinfo/><Footer/></>}/>
+      <Route path='/transfer-patient' element={<><Navbar/><TransferPatient/><Footer/></>}/>
+      <Route path='/update-bed-status' element={<><Navbar/><UpdateBedStatus/><Footer/></>}/>
+      <Route path='/update-hospital-profile' element={<><Navbar/><UpdateHospitalProfile/><Footer/></>}/>
     
     </Routes>
     <ToastContainer position="top-right" autoClose={3000} />
